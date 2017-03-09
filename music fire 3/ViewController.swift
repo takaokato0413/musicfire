0716c05b96b49bb597c492948e08c154c09dcdc4
takao kato <apple@apple-no-MacBook-Air.local>
2017-03-10 04:55:00 +0900

    Initial Commit

---
+++ music fire 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  music fire 3
+//
+//  Created by takao kato on 2017/03/10.
+//  Copyright © 2017年 takao kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
